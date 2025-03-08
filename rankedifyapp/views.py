f3a5e72989d18316a4585c05da84adf2bb61a5f1
Aidan <aidan.brock.mine@outlook.com>
2025-03-08 18:06:51 +0000

    Added default page to redirect to home

--- rankedifyapp/views.py
+++ rankedifyapp/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import redirect
 
+def default_page(request):
+    response = redirect('/rankedify/home')
+    return response
+
 def home(request):
     return HttpResponse("this is home")
