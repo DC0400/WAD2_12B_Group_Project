493bf94cf837c7d2a7aaad7463a1dd002baef917
Aidan <aidan.brock.mine@outlook.com>
2025-03-08 16:13:32 +0000

    Added template-page.html and jquery to apply styling to pages

---
+++ static/js/page_styling.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    //Styling used on each page
+    $("html").addClass("bg-gradient-to-b from-green-800 h-full to-green-400");
+    $("body").addClass("bg-gradient-to-b from-green-800 h-full to-green-400");
+    $(".link").addClass("font-medium text-zinc-950 underline italic hover:no-underline")
+    $(".button").addClass("bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-1 rounded")
+
+    //nav bar
+    $("nav").addClass("mx-auto bg-gray-800 flex w-full h-16 select-none");
+    $("#logo").addClass("px-4 text-green-500 font-semibold h-full flex items-center cursor-default");
+    $(".nav-item").addClass("px-4 text-green-50 font-semibold h-full cursor-pointer hover:bg-green-600 transition duration-300 flex items-center");
+    
+    //Log_in/ create_account Styling
+    $(".login-and-create-account-form").addClass("ps-40")
+    $(".input-title").addClass("input-title w-24 text-right")
+    $("input-container").addClass("flex space-x-2")
+    $(".text-field").addClass("w-64 border rounded px-2 py-1 mb-2")
+})
