4bbbaea569f5a2d8f509c653a770f63c01cca9ce
Adrian Czerwinski <adrian@czerwinski.info.pl>
2025-03-12 12:31:12 +0000

    Basic log-in and sign-up

--- rankedify/views.py
+++ rankedify/views.py
@@ -1,3 +1,61 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rango.models import Category
+from rango.models import Page
+from rango.forms import CategoryForm
+from rango.forms import PageForm
+from django.shortcuts import redirect
+from django.urls import reverse
+from rango.forms import UserForm, UserProfileForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+# Place "@login_required" before every page function that requires log-in before access.
+
+def register(request):
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+          
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+
+            registered = True
+        else:
+            print(user_form.errors, profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    return render(request,
+        'rankedify/signup.html',
+        context = {'user_form': user_form,
+            'profile_form': profile_form,
+            'registered': registered})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect(reverse('rankedify:home'))
+            else:
+                return HttpResponse("Your Rankedify account is disabled.")
+        else:
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details.")
+    else:
+        return render(request, 'rankedify/login.html')
