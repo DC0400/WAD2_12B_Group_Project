7cd404df0d45739b05458b53d87d64a691a18d2e
Aidan <aidan.brock.mine@outlook.com>
2025-03-13 14:04:13 +0000

    updated home.html styling

--- static/js/page_styling.js
+++ static/js/page_styling.js
@@ -10,16 +10,21 @@ $(document).ready(function() {
     $("#logo").addClass("px-4 text-green-500 font-semibold h-full flex items-center cursor-default");
     $(".nav-item").addClass("px-4 text-green-50 font-semibold h-full cursor-pointer hover:bg-green-600 transition duration-300 flex items-center");
     
+
     //home.html
-    $(".leaderboard-container").addClass("w-[650px] border-[2px] border-zinc-950 mx-auto rounded bg-blue-500")
-    $(".leaderboard-categories").addClass("flex border-b-[2px] border-zinc-950")
-    $(".leaderboard-categories-option").addClass("cursor-pointer text-center w-1/3 hover:bg-blue-700")
-    $(".leaderboard-search").addClass("border-b-[2px] border-zinc-950 pt-[2px] pb-[2px]")
+    $(".leaderboard-container").addClass("w-[650px] border-[3px] border-zinc-900 mx-auto rounded bg-gray-900 mt-[5%] text-gray-300 font-semibold")
+    $(".leaderboard-categories").addClass("flex border-b-[3px] border-zinc-950")
+    $(".leaderboard-categories-option").addClass("select-none cursor-pointer text-center w-1/3 hover:bg-green-700")
+    //home.html: searchbar
+    $(".leaderboard-search").addClass("border-b-[2px] pl-1 border-zinc-950 pt-[2px] pb-[2px]")
     $(".leaderboard-search-box").addClass("rounded-lg w-32 pl-2 pr-2")
-    $(".leaderboard-entry").addClass("flex space-x-4 h-full");
-    $(".leaderboard-position").addClass("w-1/6 flex-none")
-    $(".leaderboard-username").addClass("w-1/2 flex-1")
+    //home.html: entry
+    $(".leaderboard-entry").addClass("flex h-full border-b-[2px] [&_:is(span)]:pl-[3px] border-zinc-950");
+    $(".leaderboard-entry-title").addClass("border-b-[3px]")
+    $(".leaderboard-position").addClass("w-1/12 flex-none border-r-[3px] border-zinc-950")
+    $(".leaderboard-username").addClass("w-1/2 flex-1 border-r-[3px] border-zinc-950")
     $(".leaderboard-listening-time").addClass("w-1/3 flex-1")
+
 
     //Log_in.html/ signup.html Styling
     $(".login-and-create-account-form").addClass("ps-40 pt-8")
@@ -27,3 +32,15 @@ $(document).ready(function() {
     $(".input-container").addClass("flex space-x-8")
     $(".text-field").addClass("w-64 border-zinc-950 rounded px-2 py-1 mb-2")
 })
+
+//home.html leaderboard category buttons handling 
+$(".leaderboard-categories-option").click(function() {
+    let options = $(".leaderboard-categories").children();
+
+    console.log("this is running")
+    options.each(function() {
+        $(this).removeClass("bg-blue-700"); // Remove the class from all siblings
+    });
+
+    $(this).addClass("bg-blue-700"); // Add the class to the clicked element
+});
