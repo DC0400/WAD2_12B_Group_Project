4412adfb31b4d8dc8196f3557a8ececc28a9d80a
Aidan <aidan.brock.mine@outlook.com>
2025-03-18 13:16:07 +0000

    Renamed css so it can be used on profile page

--- static/js/page_styling.js
+++ static/js/page_styling.js
@@ -26,6 +26,9 @@ $(document).ready(function() {
     $(".leaderboard-username").addClass("w-1/2 flex-1 border-r-[3px] border-zinc-950")
     $(".leaderboard-listening-time").addClass("w-1/3 flex-1")
 
+    //home.html/profile.html
+    $(".page-splitter").addClass("flex h-full")
+
     //friends.html
     $(".friends").addClass("h-[100px] overflow-auto")
     $(".friends-container").addClass("w-[650px] max-h-[250px] border-[3px] border-zinc-900 mx-auto rounded bg-gray-900 mt-[5%] text-gray-300 font-semibold")
