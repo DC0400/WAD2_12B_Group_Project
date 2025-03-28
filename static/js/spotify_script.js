c6652b6686346416fc146c4dfb444bf0d4e0f26e
Daniel Corbett <2875397c@student.gla.ac.uk>
2025-03-28 12:21:03 +0000

    changed callback urls

--- static/js/spotify_script.js
+++ static/js/spotify_script.js
@@ -51,7 +51,7 @@ export async function redirectToAuthCodeFlow(clientId){
     const params = new URLSearchParams();
     params.append("client_id", clientId);
     params.append("response_type", "code");
-    params.append("redirect_uri", "http://127.0.0.1:8000/callback");
+    params.append("redirect_uri", "https://rankedify.pythonanywhere.com/callback");
     params.append("scope", "user-read-private user-read-email user-top-read user-read-recently-played");
     params.append("code_challenge_method", "S256");
     params.append("code_challenge", challenge);
@@ -85,7 +85,7 @@ export async function getAccessToken(clientId, code) {
     params.append("client_id", clientId);
     params.append("grant_type", "authorization_code");
     params.append("code", code);
-    params.append("redirect_uri", "http://127.0.0.1:8000/callback");
+    params.append("redirect_uri", "https://rankedify.pythonanywhere.com/callback");
     params.append("code_verifier", verifier);
 
     const result = await fetch("https://accounts.spotify.com/api/token", {
